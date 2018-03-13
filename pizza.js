9b1e4ece951b14a4071c6aa93fd27eba4f041778
Carter Schleif <carterschleif@Carters-MacBook-Pro.local>
2018-03-13 14:45:22 -0500

    priceChange works

--- pizza.js
+++ pizza.js
@@ -12,34 +12,38 @@ class Order {
 
 
 class Pizza {
-    constructor(type = cheese, price = 10.00) {
+    constructor(type, price = 10.00) {
       this.type = type;
       this.price = price;
       console.log('In Pizza constructor, cheese pizza:', this);
     }
+    changePrice(){
+        // super.changePrice();
+      this.price = this.price + .99;
+    }
   }
 
+  let cheesePizza = new Pizza('cheese', 10.00);
+//   console.log('CHEESE PIZZA:', cheesePizza);
 
   class Pepperoni extends Pizza {
 
     constructor(type = pepperoni, price) {
       super( type, price);
     }
-  
-    changePrice(){
-      super.changePrice();
-      this.price = price + .99;
-      console.log('Pepperoni after price change:', this.price);
-    }
   }
+
+  let pepperoniPizza = new Pepperoni('pepperoni', 10.00);
+  console.log('pepperoni before price change:', pepperoniPizza)
+pepperoniPizza.changePrice();
+console.log('pepperoni after price change:', pepperoniPizza);
+
 
   class Bacon extends Pizza {
       constructor(type = bacon, price) {
           super(type, price);
       }
-      changePrice(){
-          super.changePrice();
-          this.price = price + .99;
-          console.log('Bacon after price hange:', this.price);
-      }
   }
+
+
+  
